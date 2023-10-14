8915df696704e8c55048b76d4d6ed441d4305583
Lockl00p <hakzstudios@gmail.com>
2023-10-13 22:48:42 -0500

    Just Start over with an example as a base

---
+++ source/main.c
@@ -0,0 +1,50 @@
+/*
+#############################################
+##DS Programming Guide - From Zero To Hero!##
+####Example #2 - MODE 0 Tiled Backgrounds####
+#############################################
+*/
+
+/*
+############
+##Includes##
+############
+*/
+
+// Include C
+#include <stdio.h>
+
+// Include Libnds
+#include <nds.h>
+
+// Include NFLib
+#include <nf_lib.h>
+
+/*
+###############
+##Main(){...}##
+###############
+*/
+
+int main(int argc, char **argv) {
+
+	NF_Set2D(0, 0);				// Turn on MODE 0 on the Top Screen
+	
+	NF_SetRootFolder("NITROFS");	// Set the Root Folder
+
+	NF_InitTiledBgBuffers();	// Initialize the Tiled Backgrounds System on the Top Screen
+	NF_InitTiledBgSys(0);
+
+	// Load the Tiled Background
+	NF_LoadTiledBg("Background", "Background", 256, 256);
+
+	NF_CreateTiledBg(0, 3, "Background");
+	
+	while(1){
+	
+		swiWaitForVBlank();
+		
+	}
+	
+	return 0;
+}
